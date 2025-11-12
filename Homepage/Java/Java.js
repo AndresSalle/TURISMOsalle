f434b7cce6c5d4d43aa6d8c3c3bcea27a681ed5b
Nicolás Romero <nicolasromero@192.168.0.8>
2025-11-11 21:43:47 -0500

    Final version

---
+++ Homepage/Java/Java.js
@@ -0,0 +1,107 @@
+  function toggleHeart1() {
+    let img = document.getElementById("heartA");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+   function toggleHeart2() {
+    let img = document.getElementById("heartB");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+   function toggleHeart3() {
+    let img = document.getElementById("heartC");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+   function toggleHeart4() {
+    let img = document.getElementById("heartD");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+   function toggleHeart5() {
+    let img = document.getElementById("heartE");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+   function toggleHeart6() {
+    let img = document.getElementById("heartF");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage1() {
+    let img = document.getElementById("Gif1");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage2() {
+    let img = document.getElementById("Gif2");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage3() {
+    let img = document.getElementById("Gif3");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage4() {
+    let img = document.getElementById("Gif4");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage5() {
+    let img = document.getElementById("Gif5");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
+
+  function toggleImage6() {
+    let img = document.getElementById("Gif6");
+    if (img.style.display === "none") {
+      img.style.display = "block";
+    } else {
+      img.style.display = "none";
+    }
+  }
